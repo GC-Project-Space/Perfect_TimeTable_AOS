3d3c734864820138e0c152919cd21e443f1beef9
ShimFFF <shimseawon1510@gmail.com>
2023-12-05 12:31:09 +0900

    :sparkles: Feat : 뷰 초기화 메서드 추가

--- android/PerfectTimeTable/app/src/main/java/com/example/perfecttimetable/MainActivity.kt
+++ android/PerfectTimeTable/app/src/main/java/com/example/perfecttimetable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.perfecttimetable
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.perfecttimetable.databinding.ActivityMainBinding
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        clearView()
 
         //시간표 만들기로 넘어가기
         binding.createNewTableTxt.setOnClickListener {
@@ -35,6 +38,8 @@ class MainActivity : AppCompatActivity() {
     //시간표 정보 받아오기
     override fun onResume() {
         super.onResume()
+        clearView()
+
         val bundle = intent.getBundleExtra("bundle")
         val receivedSubList: ArrayList<Subject>? = bundle?.getParcelableArrayList("subList")
         val receivedTitle: String? = bundle?.getString("title")
@@ -53,6 +58,9 @@ class MainActivity : AppCompatActivity() {
 
     //시간표 정보 설정 함수
     private fun setContent(subList:List<Subject>){
+
+        clearView()
+
         //시간표 이름 설정
         binding.timeTableNameTxt.text=title
 
@@ -61,7 +69,7 @@ class MainActivity : AppCompatActivity() {
             for( time in item.startTime..item.endTime){
                 if(time==item.endTime){continue}
                 val resourceId = resources.getIdentifier("${item.day}_${time}", "id", packageName)
-                val textView = findViewById<TextView>(resourceId)
+                val textView = binding.root.findViewById<TextView>(resourceId)
                 textView?.text = item.name // 제목 설정
                 textView?.setTextColor(Color.parseColor("#000000")) //글자색
                 textView?.setBackgroundColor(Color.parseColor("#FFE2EFFF")) //배경색
@@ -69,4 +77,46 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    //뷰 초기화
+    private fun clearView() {
+            Log.d("뷰클리어!", "깨끗")
+
+            for (time in 1..9) {
+                val resourceId1 = resources.getIdentifier("${Constance.MONDAY}_${time}", "id", packageName)
+                val resourceId2 = resources.getIdentifier("${Constance.THURSDAY}_${time}", "id", packageName)
+                val resourceId3 = resources.getIdentifier("${Constance.WEDNESDAY}_${time}", "id", packageName)
+                val resourceId4 = resources.getIdentifier("${Constance.TUESDAY}_${time}", "id", packageName)
+                val resourceId5 = resources.getIdentifier("${Constance.FRIDAY}_${time}", "id", packageName)
+
+                val textView1 = binding.root.findViewById<TextView>(resourceId1)
+                textView1?.text = "" // Clear text
+                textView1?.setTextColor(Color.parseColor("#000000")) // Set text color to black
+                textView1?.setBackgroundColor(Color.parseColor("#FFFFFF")) // Set background color to white
+
+                val textView2 = binding.root.findViewById<TextView>(resourceId2)
+                textView2?.text = "" // Clear text
+                textView2?.setTextColor(Color.parseColor("#000000")) // Set text color to black
+                textView2?.setBackgroundColor(Color.parseColor("#FFFFFF")) // Set background color to white
+
+                val textView3 = binding.root.findViewById<TextView>(resourceId3)
+                textView3?.text = "" // Clear text
+                textView3?.setTextColor(Color.parseColor("#000000")) // Set text color to black
+                textView3?.setBackgroundColor(Color.parseColor("#FFFFFF")) // Set background color to white
+
+                val textView4 = binding.root.findViewById<TextView>(resourceId4)
+                textView4?.text = "" // Clear text
+                textView4?.setTextColor(Color.parseColor("#000000")) // Set text color to black
+                textView4?.setBackgroundColor(Color.parseColor("#FFFFFF")) // Set background color to white
+
+                val textView5 = binding.root.findViewById<TextView>(resourceId5)
+                textView5?.text = "" // Clear text
+                textView5?.setTextColor(Color.parseColor("#000000")) // Set text color to black
+                textView5?.setBackgroundColor(Color.parseColor("#FFFFFF")) // Set background color to white
+            }
+
+    }
+
+
+
 }
